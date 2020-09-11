acb551d3998ab31c385ebb990e10f8de2e01d4d0
Chris Falk <christophersfalk@gmail.com>
2020-09-11 10:23:20 -0700

    finalize local storage writing functionality. updated the loop to grab local storage and write in the text field. made the save icon bigger.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// write current day to page
+// write current day to top of page
 let currentDay = moment().format('LLLL');
 $("#currentDay").text(currentDay);
 
-//make an array of business hours so we can grab and then check them 
+//make an array of business hours so we can grab and then write them to DOM
 
 let businessHours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
 
 //  get the current hour 
 let currentHour = parseInt(moment().format('H'));
-console.log(currentHour);
-// test time condition to change color of the div 
 
+// create a loop based on business hours to generate timeblocks
 for (i = 9; i <= 17; i++) {
 
     //create Row Div and add attributes and write the row to the DOM
@@ -23,9 +22,9 @@ for (i = 9; i <= 17; i++) {
 
     let timeDiv = $('<div>');
     timeDiv.addClass("hour col");
-    
+
     let hour = $('<h4>');
-    hour.text(businessHours[i-9]);
+    hour.text(businessHours[i - 9]);
     timeDiv.append(hour);
 
     rowDiv.append(timeDiv);
@@ -33,20 +32,17 @@ for (i = 9; i <= 17; i++) {
     //create content div and add attributes and write to the row div
 
     let contentDiv = $('<textarea>');
-    contentDiv.addClass("description col-10 " + businessHours[i-9]);
+    contentDiv.addClass("description col-10 " + businessHours[i - 9]);
 
     if (i < currentHour) {
         contentDiv.addClass("past");
-    } 
-    
+    }
     if (i === currentHour) {
         contentDiv.addClass("present");
     }
-    
     if (i > currentHour) {
         contentDiv.addClass("future");
     }
-
 
     rowDiv.append(contentDiv);
 
@@ -54,30 +50,25 @@ for (i = 9; i <= 17; i++) {
 
     let buttonDiv = $('<div>');
     buttonDiv.addClass("saveBtn col");
-    buttonDiv.append("<i class=\"far fa-save " + businessHours[i-9] + "\"></i>");
+    buttonDiv.append("<i class=\"far fa-save " + businessHours[i - 9] + "\"></i>");
     rowDiv.append(buttonDiv);
 
-    //compare with current time
 
-    //create function to change color, based on time, in if statement
-
+    //grab local storage items and populate them in the loop 
+    let iClass = contentDiv.attr("class").split(" ")[2];
+    let todos = localStorage.getItem(iClass);
+    contentDiv.text(todos);
 }
 
-//event listener to unlock time slots 
-//test condition that will allow text inputs 
-//event listener for clicking on buttons
 
+//event listen to write to local storage 
 $(document).on("click", ".far", function () {
-    const iClass = $(this).attr("class").split(" ")[2]; // class from button's i
-    const whichTextarea = $("textarea." + iClass); // which textarea that matches iClass
-    const todo = whichTextarea.val();
-    console.log(todo);
 
-    //let toDo = localStorage.setItem("toDos", textarea.$(this).attr("class").split(" ")[2]))
+    const iClass = $(this).attr("class").split(" ")[2]; // grabs the class from button's i. "9am"
+    const whichTextarea = $("textarea." + iClass); // selects the textarea that has the same class from iClass above
+    const todoInput = whichTextarea.val(); //assigns the value of the text field to the const 
 
-    //if ($(this).attr("id")) 
+    let todos = localStorage.setItem(iClass, todoInput); //assigns to local storage with the time class ("9am"), as the key
+
 
 });
-
-
-//write to local storage 
